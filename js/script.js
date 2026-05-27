72d7739eff6eba518b490629b00eee6a8a5f0dd3
b13nv3n1dos12 <jjulialejandra@gmail.com>
2026-05-26 22:28:09 -0600

    listooo

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+if (menuBtn && navLinks) {
+  menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+}
+
+const contactForm = document.getElementById("contactForm");
+
+if (contactForm) {
+  contactForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById("nombre").value;
+    const correo = document.getElementById("correo").value;
+    const evento = document.getElementById("evento").value;
+    const mensaje = document.getElementById("mensaje").value;
+
+    if (nombre === "" || correo === "" || evento === "" || mensaje === "") {
+      alert("Por favor completa todos los campos.");
+      return;
+    }
+
+    const confirmar = confirm("¿Deseas enviar esta información?");
+
+    if (confirmar) {
+      alert("Mensaje enviado correctamente. Gracias por contactar a Eventos Chiapas.");
+      contactForm.reset();
+    } else {
+      alert("Envío cancelado.");
+    }
+  });
+}
